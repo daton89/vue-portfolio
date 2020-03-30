6cd0014cecce2cd2611842a2ce5748f0a20f3b2f
Tony D'Angelo <tonydangelo123@gmail.com>
2020-03-30 15:37:09 +0200

    add svg icons and component

--- src/components/Daton.vue
+++ src/components/Daton.vue
@@ -1,22 +1,49 @@
 <template>
   <div>
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="daton avatar" class="avatar" src="../assets/my-avatar.svg" />
     <h1 class="name">{{ msg }}</h1>
+    <a href="http://github.com/daton89" target="_blank" rel="noopener noreferrer">
+      <svg-icon class="social-icon" name="github" color="github" size="large" />
+    </a>
+    <a href="https://twitter.com/daton89" target="_blank" rel="noopener noreferrer">
+      <svg-icon class="social-icon" name="twitter" color="twitter" size="large" />
+    </a>
+    <a href="https://www.linkedin.com/in/tony-d-angelo-0b6b72a6/" target="_blank" rel="noopener noreferrer">
+      <svg-icon class="social-icon" name="linkedin" color="linkedin" size="large" />
+    </a>
+    <a href="https://www.xing.com/profile/Antonio_DAngelo9/cv" target="_blank" rel="noopener noreferrer">
+      <svg-icon class="social-icon" name="xing" color="xing" size="large" />
+    </a>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import Vue from 'vue'
+import SvgIcon from './SvgIcon.vue'
 
-@Component
-export default class Daton extends Vue {
-  @Prop() private msg!: string;
-}
+export default Vue.extend({
+  name: 'Daton',
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    }
+  },
+  components: {
+    SvgIcon
+  }
+})
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+.avatar {
+  width: 320px;
+}
 .name {
   color: white;
+  text-align: center;
+}
+.social-icon {
+  padding: 8px 24px;
 }
 </style>
---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,88 @@
+<template>
+  <svg viewBox="0 0 24 24" class="icon" :class="styleClasses">
+    <path :d="path" />
+  </svg>
+</template>
+
+<script>
+import Vue from 'vue'
+import icons from './icons.json'
+
+export default Vue.extend({
+  name: 'SvgIcon',
+  props: {
+    name: String,
+    size: String,
+    color: String
+  },
+  computed: {
+    path() {
+      return icons[this.name]
+    },
+    styleClasses() {
+      return [this.size, this.color].map(val => val && `is-${val}`)
+    }
+  }
+})
+</script>
+
+<style>
+/* default */
+.icon {
+  width: 1.2em;
+  height: 1.2em;
+  fill: currentColor;
+}
+
+/* sizes */
+.icon.is-large {
+  width: 2em;
+  height: 2em;
+}
+.icon.is-medium {
+  width: 1.6em;
+  height: 1.6em;
+}
+.icon.is-normal {
+  width: 1.2em;
+  height: 1.2em;
+}
+.icon.is-small {
+  width: 1em;
+  height: 1em;
+}
+.icon.is-tiny {
+  width: 0.8em;
+  height: 0.8em;
+}
+
+/* colors */
+.icon.is-twitter {
+  fill: #1da1f2;
+}
+.icon.is-linkedin {
+  fill: #0077b5;
+}
+.icon.is-github {
+  fill: #181717;
+}
+.icon.is-xing {
+  fill: #006567;
+}
+.icon.is-primary,
+.icon.is-blue {
+  fill: #2196f3;
+}
+.icon.is-success,
+.icon.is-green {
+  fill: #4caf50;
+}
+.icon.is-danger,
+.icon.is-red {
+  fill: #f44336;
+}
+.icon.is-warning,
+.icon.is-orange {
+  fill: #ff9800;
+}
+</style>
