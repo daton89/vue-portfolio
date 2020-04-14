46b203e6a625a4283a6d961cad1bd5554f673bf8
Tony D'Angelo <tonydangelo123@gmail.com>
2020-04-14 22:28:44 +0200

    lint intersect

--- src/components/Intersect.vue
+++ src/components/Intersect.vue
@@ -1,14 +1,14 @@
 <script>
-import Vue from "vue";
+import Vue from 'vue'
 
 const warn = msg => {
   if (!Vue.config.silent) {
-    console.warn(msg);
+    console.warn(msg)
   }
-};
+}
 
 export default Vue.extend({
-  name: "intersect",
+  name: 'intersect',
   abstract: true,
   props: {
     threshold: {
@@ -17,57 +17,53 @@ export default Vue.extend({
       default: () => [0, 0.2]
     },
     root: {
-      type: typeof HTMLElement !== "undefined" ? HTMLElement : Object,
+      type: typeof HTMLElement !== 'undefined' ? HTMLElement : Object,
       required: false,
       default: () => null
     },
     rootMargin: {
       type: String,
       required: false,
-      default: () => "0px 0px 0px 0px"
+      default: () => '0px 0px 0px 0px'
     }
   },
   mounted() {
     this.observer = new IntersectionObserver(
       entries => {
         if (!entries[0].isIntersecting) {
-          this.$emit("leave", [entries[0]]);
+          this.$emit('leave', [entries[0]])
         } else {
-          this.$emit("enter", [entries[0]]);
+          this.$emit('enter', [entries[0]])
         }
 
-        this.$emit("change", [entries[0]]);
+        this.$emit('change', [entries[0]])
       },
       {
         threshold: this.threshold,
         root: this.root,
         rootMargin: this.rootMargin
       }
-    );
+    )
 
     this.$nextTick(() => {
       if (this.$slots.default && this.$slots.default.length > 1) {
-        warn(
-          "[VueIntersect] You may only wrap one element in a <intersect> component."
-        );
+        warn('[VueIntersect] You may only wrap one element in a <intersect> component.')
       } else if (!this.$slots.default || this.$slots.default.length < 1) {
-        warn(
-          "[VueIntersect] You must have one child inside a <intersect> component."
-        );
-        return;
+        warn('[VueIntersect] You must have one child inside a <intersect> component.')
+        return
       }
 
-      this.observer.observe(this.$slots.default[0].elm);
-    });
+      this.observer.observe(this.$slots.default[0].elm)
+    })
   },
   destroyed() {
-    this.$emit("destroyed");
-    this.observer.disconnect();
+    this.$emit('destroyed')
+    this.observer.disconnect()
   },
   render() {
-    return this.$slots.default ? this.$slots.default[0] : null;
+    return this.$slots.default ? this.$slots.default[0] : null
   }
-});
+})
 </script>
 
 <style></style>
