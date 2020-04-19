cc9175b5d9fb58d159d4089e7f15571cd6f106b8
Tony D'Angelo <tonydangelo123@gmail.com>
2020-04-19 22:54:25 +0200

    use lazyload

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import VueLazyload from 'vue-lazyload'
 
 import App from './App.vue'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyload)
 
 new Vue({
   render: h => h(App)
