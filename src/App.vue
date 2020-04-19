403c10b9d6780d193d81119ea77039d8359196d4
Tony D'Angelo <tonydangelo123@gmail.com>
2020-04-19 18:02:45 +0200

    clean up app with main css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    <carousel perPage="1" navigationEnabled navigationNextLabel=">" navigationPrevLabel="<">
+    <carousel :perPage="1" :autoplay="true" :autoplayTimeout="10000" :autoplayHoverPause="true">
       <slide>
         <Daton msg="I'm daton89" />
       </slide>
       <slide>
         <Bio />
+      </slide>
+      <slide>
+        <m-mongo />
       </slide>
     </carousel>
   </div>
@@ -13,40 +16,25 @@
 
 <script lang="ts">
 import Vue from 'vue'
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
 import { Carousel, Slide } from 'vue-carousel'
-import Bio from '@/components/Bio.vue'
-
 import Daton from './components/Daton.vue'
+import Bio from '@/components/Bio.vue'
+import MMongo from '@/components/MMongo.vue'
 
 export default Vue.extend({
   components: {
     Carousel,
     Slide,
     Daton,
-    Bio
+    Bio,
+    MMongo
   }
 })
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Fira+Code&display=swap');
-
-html,
-body {
-  font-family: 'Fira Code', monospace;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  height: 100%;
-  background-size: 140vw 140vh;
-  background-color: #fff;
-  background-image: linear-gradient(225deg, #ff0000, #cc00cc, #1800dd);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+@import url('./assets/css/main.css');
 </style>
