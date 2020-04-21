6ba8966513b6d39424ed78368e2450d7d11cd6e5
Tony D'Angelo <tonydangelo123@gmail.com>
2020-04-21 20:15:21 +0200

    move js for blix widget

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,9 @@ Vue.use(VueLazyload, {
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// Add Blix Widget
+const blixWidget = document.getElementById('widget-1') as HTMLScriptElement
+if (blixWidget) {
+  blixWidget.src = `https://wgh.onblix.com/resource/widget_helper.js?widget_id=azqiceOaRbsgVuatXMrh&redirect_uri_ext=${window.location.href}`
+}
